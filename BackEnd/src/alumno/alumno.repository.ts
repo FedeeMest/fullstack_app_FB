a5f6c01023d3b6f763765bc02792e5187337e99e
belengst <belen14guastoni@gmail.com>
2024-07-25 02:47:14 -0300

    Update Alumno CRUD

--- BackEnd/src/alumno/alumno.repository.ts
+++ BackEnd/src/alumno/alumno.repository.ts
@@ -1,11 +1,13 @@
 import { Repository} from "../shared/repository.js";
 import { Alumno } from "./alumno.entity.js";
 import { pool } from "../shared/db/conn.mysql.js";
+import { ResultSetHeader, RowDataPacket } from 'mysql2'
 
 
 export class AlumnoRepository implements Repository <Alumno> {
     public async findAll(): Promise<Alumno[] | undefined> {
-        throw new Error("Method not implemented.");
+        const [alumnos] = await pool.query("select * from alumnos");
+        return alumnos as Alumno[];
     }
     public async findOne(item: { id: string; }): Promise<Alumno | undefined> {
         throw new Error("Method not implemented.");
