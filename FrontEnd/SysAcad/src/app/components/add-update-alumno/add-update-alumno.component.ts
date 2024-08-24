e1c3f3112aaef34c0da672da1cb853a9633b472a
Fede Mestre <fede.mestre01@gmail.com>
2024-08-24 01:11:53 -0300

    Avances FrontEnd

---
+++ FrontEnd/SysAcad/src/app/components/add-update-alumno/add-update-alumno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-update-alumno',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-update-alumno.component.html',
+  styleUrl: './add-update-alumno.component.css'
+})
+export class AddUpdateAlumnoComponent {
+
+}
