38d1ca2477cf6f21707aafb275bf93957b782091
belengst <belen14guastoni@gmail.com>
2024-08-24 03:08:08 -0300

    Avances FrontEnd

--- FrontEnd/SysAcad/src/app/app.routes.ts
+++ FrontEnd/SysAcad/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AlumnosComponent } from './components/alumnos/alumnos.component';
+import { AddUpdateAlumnoComponent } from './components/add-update-alumno/add-update-alumno.component';
 
 export const routes: Routes = [
     {path: " ", component: AppComponent},
-    {path: "alumnos", component: AlumnosComponent}
+    {path: "alumnos", component: AlumnosComponent},
+    {path: "add_alumno", component: AddUpdateAlumnoComponent}
 ];
