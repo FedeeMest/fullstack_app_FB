0195839790328e332bb892be9a47ce492eccc0e4
Fede_Mest <fede.mestre01@gmail.com>
2024-08-25 03:54:14 -0300

    FrontEnd (avances estéticos)

--- FrontEnd/SysAcad/src/app/app.routes.ts
+++ FrontEnd/SysAcad/src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { AlumnosComponent } from './components/Alum/alumnos/alumnos.component';
 import { AddUpdateAlumnoComponent } from './components/Alum/add-update-alumno/add-update-alumno.component';
 import { MateriasComponent } from './components/Mat/materias/materias.component';
 import { AddUpdateMateriaComponent } from './components/Mat/add-update-materia/add-update-materia.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 export const routes: Routes = [
-    {path: " ", component: AppComponent},
+    {path: "", component: InicioComponent},
     {path: "alumnos", component: AlumnosComponent},
     {path: "add_alumno", component: AddUpdateAlumnoComponent},
     {path: "materias", component: MateriasComponent},
