0a97e134a6e4d8e842da6af8937303043cbccb17
Fede_Mest <fede.mestre01@gmail.com>
2024-08-26 00:04:59 -0300

    Pureba Front

--- FrontEnd/SysAcad/src/app/app.component.ts
+++ FrontEnd/SysAcad/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule} from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule,NavbarComponent],
+  imports: [RouterModule,NavbarComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
