725e574acb010f01f2801c86d4c09186a29326f0
Fede_Mest <fede.mestre01@gmail.com>
2024-11-01 15:00:37 -0300

    first_test

--- BackEnd/src/inscripcion/inscripcion.entity.ts
+++
@@ -1,8 +0,0 @@
-export class Inscripcion {
-    constructor(
-        public alumno_id: number,
-        public materia_id: number,
-        public fecha: string,
-        public id?: number
-    ){}
-}
--- BackEnd/src/materia/materia.entity.ts
+++
@@ -1,8 +0,0 @@
-export class Materia {
-    constructor(
-        public nombre: string,
-        public horas_anuales: number,
-        public modalidad: string,
-        public id?: number
-    ){}
-}
