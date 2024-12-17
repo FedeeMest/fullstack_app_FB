841c0c3c3b69d06ad6f346f6132f61898d11ff89
belenght <belen14guastoni@gmail.com>
2024-12-17 02:31:53 -0300

    auth_update

--- BackEnd/src/app.ts
+++ BackEnd/src/app.ts
@@ -7,8 +7,8 @@ import { materiaRouter } from './materia/materia.routes.js';
 import { inscripcionRouter } from './inscripcion/inscripcion.routes.js';
 import cors from 'cors';
 import { authRoutes } from './auth/auth.routes.js';
-import { verifyRole } from '../src/Middleware/authMiddleware.js'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
-
+import { verifyRole } from './Middleware/authMiddleware.js'; // Asegúrate de ajustar la ruta según tu estructura de proyecto
+import { adminRouter } from './admin/admin.routes.js';
 
 
 const app = express();
@@ -21,9 +21,13 @@ app.use ((req, res, next) => {
 
 app.use('/auth', authRoutes);
 
-app.use('/api/alumnos', verifyRole(['admin']), alumnoRouter);
+// Rutas protegidas para admin
+app.use('/api/admins', verifyRole(['admin']), adminRouter);
 app.use('/api/materias', verifyRole(['admin']), materiaRouter);
-app.use('/api/inscripciones', verifyRole(['admin']), inscripcionRouter);
+
+// Rutas protegidas para admin y alumno
+app.use('/api/alumnos', verifyRole(['admin', 'alumno']), alumnoRouter);
+app.use('/api/inscripciones', verifyRole(['admin', 'alumno']), inscripcionRouter);
 
 
 app.use((_, res) => {
