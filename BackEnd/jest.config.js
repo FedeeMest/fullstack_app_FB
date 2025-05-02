2b3b5a984a91753edebf279827eb55da24146625
Fede_Mest <fede.mestre01@gmail.com>
2025-05-02 12:28:38 -0300

    fix_back

---
+++ BackEnd/jest.config.js
@@ -0,0 +1,9 @@
+const config = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
+    moduleFileExtensions: ['ts', 'js'],
+    rootDir: './src',
+};
+export default config;
+//# sourceMappingURL=jest.config.js.map
